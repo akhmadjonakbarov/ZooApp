b73b4e6fce0903efb06be8bca84c0468c01b5eb0
Akhmadjon Akbarov <97084545+akhmadjonakbarov@users.noreply.github.com>
2025-01-29 02:45:41 +0500

    ref: animalmodel was updated

--- lib/app/features/animal/models/animal.dart
+++ lib/app/features/animal/models/animal.dart
@@ -1,11 +1,15 @@
 class Animal {
   final String title;
   final String description;
+  final String subTitle;
   final String imgUrl;
+  final List<String> status;
 
   Animal({
     required this.title,
+    required this.subTitle,
     required this.description,
+    required this.status,
     required this.imgUrl,
   });
 }
